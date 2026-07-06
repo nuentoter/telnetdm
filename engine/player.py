8a4f3492e57beb74c756b5f8e9a8151ec70ecfe6
nuentoter <nuentoter@gmail.com>
2026-07-06 00:38:30 -0400

    Add basic ability scores to player class

--- engine/player.py
+++ engine/player.py
@@ -3,7 +3,6 @@ class Player:
         self.name = "Wanderer"
 
         self.level = 1
-        self.experience = 0
 
         self.max_hp = 10
         self.current_hp = 10
@@ -11,3 +10,11 @@ class Player:
         self.room = "start_room"
 
         self.inventory = []
+
+        # Basic ability scores (very simplified D&D-style)
+        self.strength = 10
+        self.dexterity = 10
+        self.constitution = 10
+        self.intelligence = 10
+        self.wisdom = 10
+        self.charisma = 10
