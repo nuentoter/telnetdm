c3ad5ad79e9d7a8edb0b632be78104577427f399
nuentoter <nuentoter@gmail.com>
2026-07-06 00:09:05 -0400

    Add Player class with initial attributes

---
+++ engine/player.py
@@ -0,0 +1,13 @@
+class Player:
+    def __init__(self):
+        self.name = "Wanderer"
+
+        self.level = 1
+        self.experience = 0
+
+        self.max_hp = 10
+        self.current_hp = 10
+
+        self.room = "start_room"
+
+        self.inventory = []
