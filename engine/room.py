5cd3ad71ca45b5444874d3a244d1c8ed4d5ab1d2
nuentoter <nuentoter@gmail.com>
2026-07-07 10:20:42 -0400

    Add NPCs handling to room description

--- engine/room.py
+++ engine/room.py
@@ -7,38 +7,75 @@ class Room:
         description,
         exits=None,
         items=None,
+        npcs=None,
         hidden=None,
     ):
 
         self.id = room_id
+
         self.name = name
+
         self.description = description
 
         self.exits = exits or {}
+
         self.items = items or []
+
+        self.npcs = npcs or []
+
         self.hidden = hidden or {}
+
 
 
     def describe(self):
 
         lines = []
 
+
         lines.append(self.name)
+
         lines.append(self.description)
+
 
         if self.exits:
 
             lines.append(
-                "Exits: " +
-                ", ".join(self.exits.keys())
+                "Exits: "
+                +
+                ", ".join(
+                    self.exits.keys()
+                )
             )
+
 
         if self.items:
 
             lines.append("")
-            lines.append("You see:")
+
+            lines.append(
+                "You see:"
+            )
 
             for item in self.items:
-                lines.append(f" - {item}")
+
+                lines.append(
+                    f" - {item}"
+                )
+
+
+        if self.npcs:
+
+            lines.append("")
+
+            lines.append(
+                "People here:"
+            )
+
+            for npc in self.npcs:
+
+                lines.append(
+                    f" - {npc}"
+                )
+
 
         return "\r\n".join(lines)
