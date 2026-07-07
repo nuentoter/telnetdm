c15ed0e5c91a3d6be2b6f4e774f15bb74e3946d2
nuentoter <nuentoter@gmail.com>
2026-07-06 23:29:34 -0400

    Add normalize_words function for text normalization

--- engine/resolver.py
+++ engine/resolver.py
@@ -1,18 +1,55 @@
+def normalize_words(text):
+    """
+    Makes comparisons less strict.
+    """
+
+    replacements = {
+        "rusty": "rusted",
+        "old": "",
+        "ancient": "",
+        "thing": "",
+        "object": ""
+    }
+
+    words = text.lower().split()
+
+    cleaned = []
+
+    for word in words:
+        if word in replacements:
+            replacement = replacements[word]
+
+            if replacement:
+                cleaned.append(replacement)
+
+        else:
+            cleaned.append(word)
+
+    return cleaned
+
+
+
 def resolve_item(target, available_items):
 
-    target = target.lower()
+    target_words = normalize_words(target)
+
 
     for item in available_items:
 
-        item_words = item.lower().split()
+        item_words = normalize_words(item)
+
 
         matches = 0
 
-        for word in item_words:
-            if word in target:
+
+        for word in target_words:
+
+            if word in item_words:
                 matches += 1
 
-        if matches > 0:
+
+        if matches >= len(target_words):
             return item
+
 
     return None
