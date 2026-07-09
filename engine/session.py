5148254a23a794cda498e3f419dc065748c9f255
nuentoter <nuentoter@gmail.com>
2026-07-09 10:43:16 -0400

    Add combat attribute to Session class
    Added combat attribute to Session class and fixed formatting.

--- engine/session.py
+++ engine/session.py
@@ -2,17 +2,23 @@ from engine.player import Player
 from engine.player_storage import load_player, apply_saved_player
 
 
+
 class Session:
 
 
     def __init__(
+
         self,
+
         writer
+
     ):
 
         self.writer = writer
 
         self.player = Player()
+
+        self.combat = None
 
 
         saved = load_player()
@@ -31,8 +37,11 @@ class Session:
 
 
     def send(
+
         self,
+
         text
+
     ):
 
         self.writer.write(
