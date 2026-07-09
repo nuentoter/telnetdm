1b7ac1baf8d073566f7bf173b8a6a4da6f490ba1
nuentoter <nuentoter@gmail.com>
2026-07-09 10:18:45 -0400

    Refactor Player class to use Stats and improve methods

--- engine/player.py
+++ engine/player.py
@@ -1,54 +1,75 @@
+from engine.stats import Stats
+
+
 class Player:
 
-    def __init__(self, name="Adventurer"):
 
-        self.id = None
+    def __init__(
+
+        self,
+
+        name="Adventurer",
+
+        class_name="fighter"
+
+    ):
 
         self.name = name
 
+        self.class_name = class_name
+
         self.room = "start_room"
 
-        self.inventory = []
-
-
-        # Future D&D style data
 
         self.level = 1
 
         self.experience = 0
 
 
-        self.stats = {
-            "strength": 10,
-            "dexterity": 10,
-            "constitution": 10,
-            "intelligence": 10,
-            "wisdom": 10,
-            "charisma": 10
-        }
+        self.stats = Stats()
 
 
-        self.class_name = None
+        self.max_hp = 10
+
+        self.hp = self.max_hp
 
 
-
-    def add_item(self, item):
-
-        self.inventory.append(item)
+        self.inventory = []
 
 
 
-    def remove_item(self, item):
+    def add_item(
+        self,
+        item
+    ):
 
-        if item in self.inventory:
+        self.inventory.append(
+            item
+        )
 
-            self.inventory.remove(item)
+
+
+    def gain_xp(
+        self,
+        amount
+    ):
+
+        self.experience += amount
+
+
+        if self.experience >= self.level * 100:
+
+            self.level_up()
 
 
 
-    def has_item(self, item):
+    def level_up(self):
 
-        return item in self.inventory
+        self.level += 1
+
+        self.max_hp += 5
+
+        self.hp = self.max_hp
 
 
 
@@ -56,20 +77,28 @@ class Player:
 
         return {
 
-            "id": self.id,
-
             "name": self.name,
 
-            "room": self.room,
+            "class": self.class_name,
 
-            "inventory": self.inventory,
+            "room": self.room,
 
             "level": self.level,
 
             "experience": self.experience,
 
-            "stats": self.stats,
+            "hp": self.hp,
 
-            "class": self.class_name
+            "max_hp": self.max_hp,
+
+            "stats": self.stats.describe(),
+
+            "inventory": [
+
+                item.id
+
+                for item in self.inventory
+
+            ]
 
         }
