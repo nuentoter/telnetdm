dba29e894328d4cdd338ea4e21b3b3c2689f93a2
nuentoter <nuentoter@gmail.com>
2026-07-09 10:42:31 -0400

    Add encounter checking functionality

---
+++ engine/encounter.py
@@ -0,0 +1,44 @@
+import random
+
+from engine.creatures import create_creature
+
+
+ENCOUNTERS = {
+
+    "forest_edge": [
+
+        "forest_wolf"
+
+    ]
+
+}
+
+
+
+def check_encounter(
+    room_id
+):
+
+    creatures = ENCOUNTERS.get(
+        room_id
+    )
+
+
+    if not creatures:
+
+        return None
+
+
+    if random.random() > 0.35:
+
+        return None
+
+
+    creature_id = random.choice(
+        creatures
+    )
+
+
+    return create_creature(
+        creature_id
+    )
