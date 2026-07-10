9cc580f510b04f7c0abcbcc58ab7369375e740a5
nuentoter <nuentoter@gmail.com>
2026-07-09 22:13:15 -0400

    Refactor player_attack and enemy_attack methods

--- engine/combat.py
+++ engine/combat.py
@@ -1,12 +1,17 @@
 from engine.dice import roll
 
 
+
 class Combat:
 
     def __init__(
+
         self,
+
         player,
+
         enemy
+
     ):
 
         self.player = player
@@ -14,49 +19,70 @@ class Combat:
         self.round = 1
 
 
+
     def player_attack(self):
 
         attack_roll = roll(20)[0]
 
-        attack_total = (
-            attack_roll
-            + self.player.stats.modifier(
+
+        strength_bonus = (
+
+            self.player.stats.modifier(
                 "strength"
             )
+
         )
 
-        critical = (
-            attack_roll == 20
+
+        attack_total = (
+
+            attack_roll
+
+            +
+
+            strength_bonus
+
         )
+
+
+        critical = (
+
+            attack_roll == 20
+
+        )
+
 
         weapon = self.player.equipment.get(
             "weapon"
         )
 
+
         if weapon:
 
-            die_size = max(
-                2,
+            damage = roll(
                 weapon.damage
-            )
+            )[0]
 
         else:
 
-            die_size = 4
+            damage = roll(4)[0]
 
-        damage = roll(
-            die_size
-        )[0]
+
 
         if critical:
 
             damage *= 2
 
+
+
         hit = attack_total >= 10
+
+
 
         if hit:
 
             self.enemy.hp -= damage
+
 
         return {
 
@@ -66,7 +92,7 @@ class Combat:
 
             "attack_roll": attack_roll,
 
-            "damage": damage,
+            "damage": damage if hit else 0,
 
             "enemy_hp": max(
                 self.enemy.hp,
@@ -76,19 +102,39 @@ class Combat:
         }
 
 
+
     def enemy_attack(self):
+
+        if self.enemy.hp <= 0:
+
+            return {
+
+                "hit": False,
+
+                "damage": 0,
+
+                "player_hp": self.player.hp
+
+            }
+
+
 
         attack_roll = roll(20)[0]
 
+
         hit = attack_roll >= 8
 
+
         damage = 0
+
 
         if hit:
 
             damage = roll(4)[0]
 
             self.player.hp -= damage
+
+
 
         return {
 
@@ -99,11 +145,15 @@ class Combat:
             "damage": damage,
 
             "player_hp": max(
+
                 self.player.hp,
+
                 0
+
             )
 
         }
+
 
 
     def finished(self):
