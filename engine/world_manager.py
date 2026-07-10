faa375e788b1ed5c2a78a78af53b050b98cd104e
nuentoter <nuentoter@gmail.com>
2026-07-10 18:00:26 -0400

    Refactor WorldManager methods for consistency

--- engine/world_manager.py
+++ engine/world_manager.py
@@ -2,13 +2,9 @@ from engine.world_database import WorldDatabase
 from engine.world_seed import seed_world
 
 
-
 class WorldManager:
 
-    def __init__(
-        self,
-        registry
-    ):
+    def __init__(self, registry):
 
         self.registry = registry
 
@@ -19,32 +15,28 @@ class WorldManager:
         self.current_world = self.database.data
 
 
+        # force first room
+        if not self.database.room_exists("forest_edge"):
 
-    def get_room(
-        self,
-        room_id
-    ):
+            seed_world()
 
-        room = self.database.get_room(
-            room_id
-        )
+
+
+    def get_room(self, room_id):
+
+        room = self.database.get_room(room_id)
 
 
         if room is None:
 
-            return self.create_unknown_room(
-                room_id
-            )
+            return self.create_unknown_room(room_id)
 
 
         return room
 
 
 
-    def create_unknown_room(
-        self,
-        room_id
-    ):
+    def create_unknown_room(self, room_id):
 
         room = {
 
@@ -77,25 +69,18 @@ class WorldManager:
 
 
 
-    def room_exists(
-        self,
-        room_id
-    ):
+    def room_exists(self, room_id):
 
-        return self.database.room_exists(
-            room_id
-        )
+        return self.database.room_exists(room_id)
 
 
 
-    def move_player(
-        self,
-        player,
-        direction
-    ):
+    def move_player(self, player, direction):
 
         room = self.get_room(
+
             player.room
+
         )
 
 
@@ -107,12 +92,12 @@ class WorldManager:
         player.room = room["exits"][direction]
 
 
-        if not self.room_exists(
-            player.room
-        ):
+        if not self.room_exists(player.room):
 
             self.create_unknown_room(
+
                 player.room
+
             )
 
 
@@ -120,41 +105,22 @@ class WorldManager:
 
 
 
-    def add_item(
-        self,
-        room_id,
-        item
-    ):
+    def add_item(self, room_id, item):
 
-        room = self.get_room(
-            room_id
-        )
+        room = self.get_room(room_id)
 
-
-        room["items"].append(
-            item
-        )
+        room["items"].append(item)
 
         self.database.save()
 
 
 
-    def remove_item(
-        self,
-        room_id,
-        item
-    ):
+    def remove_item(self, room_id, item):
 
-        room = self.get_room(
-            room_id
-        )
-
+        room = self.get_room(room_id)
 
         if item in room["items"]:
 
-            room["items"].remove(
-                item
-            )
-
+            room["items"].remove(item)
 
         self.database.save()
