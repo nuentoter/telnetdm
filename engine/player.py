9d90ff9e54d1ed67dd4955225718e7ac541bac7f
nuentoter <nuentoter@gmail.com>
2026-07-09 21:30:28 -0400

    Refactor Player class and initialize equipment

--- engine/player.py
+++ engine/player.py
@@ -1,104 +1,25 @@
-from engine.stats import Stats
+self.inventory = []
 
+self.equipment = {
 
-class Player:
+    "head": None,
 
+    "chest": None,
 
-    def __init__(
+    "legs": None,
 
-        self,
+    "feet": None,
 
-        name="Adventurer",
+    "hands": None,
 
-        class_name="fighter"
+    "weapon": None,
 
-    ):
+    "offhand": None,
 
-        self.name = name
+    "ring_left": None,
 
-        self.class_name = class_name
+    "ring_right": None,
 
-        self.room = "start_room"
+    "neck": None
 
-
-        self.level = 1
-
-        self.experience = 0
-
-
-        self.stats = Stats()
-
-
-        self.max_hp = 10
-
-        self.hp = self.max_hp
-
-
-        self.inventory = []
-
-
-
-    def add_item(
-        self,
-        item
-    ):
-
-        self.inventory.append(
-            item
-        )
-
-
-
-    def gain_xp(
-        self,
-        amount
-    ):
-
-        self.experience += amount
-
-
-        if self.experience >= self.level * 100:
-
-            self.level_up()
-
-
-
-    def level_up(self):
-
-        self.level += 1
-
-        self.max_hp += 5
-
-        self.hp = self.max_hp
-
-
-
-    def describe(self):
-
-        return {
-
-            "name": self.name,
-
-            "class": self.class_name,
-
-            "room": self.room,
-
-            "level": self.level,
-
-            "experience": self.experience,
-
-            "hp": self.hp,
-
-            "max_hp": self.max_hp,
-
-            "stats": self.stats.describe(),
-
-            "inventory": [
-
-                item.id
-
-                for item in self.inventory
-
-            ]
-
-        }
+}
