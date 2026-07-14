8fa2aa25d8d6b71b336fb5a2ff9a0f2b690551c3
nuentoter <nuentoter@gmail.com>
2026-07-14 06:10:07 -0400

    Implement NPC class with dialogue and quest handling

---
+++ engine/npc.py
@@ -0,0 +1,58 @@
+class NPC:
+
+    def __init__(
+        self,
+        npc_id,
+        name,
+        personality="neutral",
+        dialogue=None,
+        quests=None
+    ):
+
+        self.id = npc_id
+        self.name = name
+        self.personality = personality
+
+        self.dialogue = dialogue or [
+            "The NPC has nothing to say."
+        ]
+
+        self.quests = quests or []
+
+
+    def talk(self):
+
+        if self.quests:
+
+            quest = self.quests[0]
+
+            return (
+                f"{self.name}:\r\n"
+                f"\"{quest['description']}\"\r\n"
+                f"Quest offered: {quest['name']}"
+            )
+
+        return (
+            f"{self.name}:\r\n"
+            f"\"{self.dialogue[0]}\""
+        )
+
+
+    def add_quest(
+        self,
+        quest
+    ):
+
+        self.quests.append(
+            quest
+        )
+
+
+    def describe(self):
+
+        return {
+            "id": self.id,
+            "name": self.name,
+            "personality": self.personality,
+            "quests": self.quests
+        }
