65ab80a4f5c1eb0cad2ef01a9494c4e097f62da3
nuentoter <nuentoter@gmail.com>
2026-07-14 08:59:25 -0400

    Add persistent quest lookup helpers

--- engine/world_database.py
+++ engine/world_database.py
@@ -11,7 +11,6 @@ WORLD_VERSION = 1
 class WorldDatabase:
 
     def __init__(self):
-
         self.data = {
             "version": WORLD_VERSION,
             "rooms": {},
@@ -19,130 +18,82 @@ class WorldDatabase:
             "quests": {},
             "events": {}
         }
-
         self.load()
 
 
     def load(self):
-
         if not os.path.exists(WORLD_FILE):
             self.save()
             return
 
         try:
-            with open(
-                WORLD_FILE,
-                "r"
-            ) as file:
+            with open(WORLD_FILE, "r") as file:
                 self.data = json.load(file)
 
         except json.JSONDecodeError:
-
             if os.path.exists(BACKUP_FILE):
-
-                shutil.copy(
-                    BACKUP_FILE,
-                    WORLD_FILE
-                )
-
-                with open(
-                    WORLD_FILE,
-                    "r"
-                ) as file:
+                shutil.copy(BACKUP_FILE, WORLD_FILE)
+                with open(WORLD_FILE, "r") as file:
                     self.data = json.load(file)
-
             else:
                 self.save()
-
 
         if "version" not in self.data:
             self.data["version"] = WORLD_VERSION
 
+        self.data.setdefault("npcs", {})
+        self.data.setdefault("quests", {})
+
 
     def save(self):
-
-        serialized = self.serialize(
-            self.data
-        )
+        serialized = self.serialize(self.data)
 
         if os.path.exists(WORLD_FILE):
-            shutil.copy(
-                WORLD_FILE,
-                BACKUP_FILE
-            )
+            shutil.copy(WORLD_FILE, BACKUP_FILE)
 
-        with open(
-            WORLD_FILE,
-            "w"
-        ) as file:
-            json.dump(
-                serialized,
-                file,
-                indent=4
-            )
+        with open(WORLD_FILE, "w") as file:
+            json.dump(serialized, file, indent=4)
 
 
     def serialize(self, value):
-
         if isinstance(value, dict):
-            return {
-                key: self.serialize(item)
-                for key, item in value.items()
-            }
+            return {key: self.serialize(item) for key, item in value.items()}
 
         if isinstance(value, list):
-            return [
-                self.serialize(item)
-                for item in value
-            ]
+            return [self.serialize(item) for item in value]
 
         if hasattr(value, "__dict__"):
-            return {
-                key: self.serialize(item)
-                for key, item in value.__dict__.items()
-            }
+            return {key: self.serialize(item) for key, item in value.__dict__.items()}
 
         return value
 
 
-    def room_exists(
-        self,
-        room_id
-    ):
+    def room_exists(self, room_id):
         return room_id in self.data["rooms"]
 
 
-    def get_room(
-        self,
-        room_id
-    ):
-        return self.data["rooms"].get(
-            room_id
-        )
+    def get_room(self, room_id):
+        return self.data["rooms"].get(room_id)
 
 
-    def add_room(
-        self,
-        room_id,
-        room_data
-    ):
+    def add_room(self, room_id, room_data):
         self.data["rooms"][room_id] = room_data
         self.save()
 
 
-    def add_npc(
-        self,
-        npc_id,
-        npc_data
-    ):
+    def add_npc(self, npc_id, npc_data):
         self.data["npcs"][npc_id] = npc_data
         self.save()
 
 
-    def add_quest(
-        self,
-        quest_id,
-        quest_data
-    ):
+    def get_npc(self, npc_id):
+        return self.data["npcs"].get(npc_id)
+
+
+    def add_quest(self, quest_id, quest_data):
         self.data["quests"][quest_id] = quest_data
         self.save()
+
+
+    def get_quest(self, quest_id):
+        return self.data["quests"].get(quest_id)
