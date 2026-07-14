e045f584f62147956de4094fad8fb1bd42793aa3
nuentoter <nuentoter@gmail.com>
2026-07-14 08:59:37 -0400

    Persist generated NPC quests

--- engine/npc.py
+++ engine/npc.py
@@ -1,35 +1,37 @@
 from engine.quest_generator import QuestGenerator
+import uuid
 
 
 class NPC:
 
-    def __init__(
-        self,
-        npc_id,
-        name,
-        personality="neutral",
-        dialogue=None,
-        quests=None
-    ):
-
+    def __init__(self, npc_id, name, personality="neutral", dialogue=None, quests=None, database=None):
         self.id = npc_id
         self.name = name
         self.personality = personality
-        self.dialogue = dialogue or [
-            "The NPC has nothing to say."
-        ]
+        self.dialogue = dialogue or ["The NPC has nothing to say."]
         self.quests = quests or []
+        self.database = database
         self.quest_generator = QuestGenerator()
 
 
     def talk(self):
 
         if not self.quests:
-            quest = self.quest_generator.generate(
-                self,
-                self.name
-            )
+            quest = self.quest_generator.generate(self, self.name)
+            quest_id = "quest_" + str(uuid.uuid4())[:8]
+
             self.quests.append(quest)
+
+            if self.database:
+                self.database.add_quest(
+                    quest_id,
+                    quest
+                )
+
+                self.database.add_npc(
+                    self.id,
+                    self.describe()
+                )
 
         quest = self.quests[0]
 
