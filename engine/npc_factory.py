5115bb5bcae1d5353186b6004c14f050ff1623a4
nuentoter <nuentoter@gmail.com>
2026-07-14 06:25:16 -0400

    Add NPC and quest object factory

---
+++ engine/npc_factory.py
@@ -0,0 +1,44 @@
+from engine.npc import NPC
+from engine.quest import Quest
+
+
+def create_quest(data):
+
+    if isinstance(data, Quest):
+        return data
+
+    return Quest(
+        name=data.get("name", "Unknown Quest"),
+        description=data.get("description", ""),
+        objective=data.get("objective", ""),
+        target=data.get("target", ""),
+        amount=data.get("amount", 1),
+        reward=data.get("reward", {})
+    )
+
+
+def create_npc(data):
+
+    if isinstance(data, NPC):
+        return data
+
+    quests = [
+        create_quest(q)
+        for q in data.get("quests", [])
+    ]
+
+    return NPC(
+        npc_id=data.get("id", "unknown"),
+        name=data.get("name", "Unknown"),
+        personality=data.get("personality", "neutral"),
+        dialogue=data.get("dialogue", []),
+        quests=quests
+    )
+
+
+def create_npcs(data):
+
+    return [
+        create_npc(npc)
+        for npc in data or []
+    ]
